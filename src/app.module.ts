2c5bc95f45bfd73b83c6ddbcf754fdf484c97172
kravchuk-st <qkp@ukr.net>
2025-06-03 00:40:57 +0300

    feat: add fav

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { UsersModule } from './users/users.module';
 import { TracksModule } from './tracks/tracks.module';
 import { ArtistsModule } from './artists/artists.module';
 import { AlbumsModule } from './albums/albums.module';
+import { FavsModule } from './favs/favs.module';
 
 @Controller()
 class RootController {
@@ -26,6 +27,7 @@ class RootController {
     TracksModule,
     ArtistsModule,
     AlbumsModule,
+    FavsModule,
   ],
   controllers: [RootController],
 })
